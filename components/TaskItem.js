e6e12bc69b511c03c980d2b6c546f9fd7772565b
JoniMop <jonatangurari@gmail.com>
2024-02-14 13:55:57 -0600

    Adding highlight effect when task is pressed

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -2,12 +2,12 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function TaskItem(props){
 	return (
-	<Pressable onPress={props.onDeleteTask.bind(this, props.item.id)}>	
           <View style={styles.taskItemStyle}>
+            <Pressable android_ripple={{color: '#000000'}}  onPress={props.onDeleteTask.bind(this, props.item.id)}>	
 	    <Text style={styles.taskItemTextStyle}>{props.index + 1}: {props.item.text}</Text>
 	    <Text style={styles.taskItemTextStyle}>{props.item.id}</Text>
-          </View>
-	</Pressable>
+             </Pressable>
+	   </View>
 	)
 }
 
@@ -16,12 +16,12 @@ export default TaskItem;
 const styles = StyleSheet.create({
 	taskItemStyle: {
 		margin: 6,
-		padding: 10,
 		borderRadius: 10,
 		backgroundColor: '#5f1aca'
 	},
 	taskItemTextStyle: {
-		color: '#ffffff'
+		color: '#ffffff',
+		padding: 10
 	}
 
 	})
